2be7a33cfa8c42bb0b09ee68a4129165c3a95987
Jakub Pacewicz <pecet3107@gmail.coim>
2022-11-10 00:05:26 +0100

    improved script for better readable and added comments

--- js/script.js
+++ js/script.js
@@ -1,66 +1,92 @@
-const formElement = document.querySelector(".js-form");
-const resultField = document.querySelector(".js-resultField");
+{
+  // variables needed multiple
 
-const converterToPLN = (amount, rate) => {
-  let result = amount * rate;
-  resultField.innerText = `${result.toFixed(2)} PLN`;
-};
-const converterFromPLN = (amount, rate, currency) => {
-  let result = amount / rate;
-  resultField.innerText = `${result.toFixed(2)} ${currency}`;
-};
-
-formElement.addEventListener("input", (event) => {
-  // event.preventDefault(); it is not necessary, when use an input
-  const currency = document.querySelector(".js-currency");
-  const amount = document.querySelector(".js-amount");
-  const amountCurrencyField = document.querySelector(".js-amountCurrencyField");
-  const currencyField = document.querySelector(".js-currencyField");
   const operationFromPLN = document.querySelector(".js-operationFromPLN");
   const operationToPLN = document.querySelector(".js-operationToPLN");
+  const amount = document.querySelector(".js-amount");
+  const currency = document.querySelector(".js-currency");
+  const formElement = document.querySelector(".js-form");
 
-  const rateEUR = 4.7;
-  const rateGBP = 5.38;
-  const rateUSD = 4.82;
-  currencyField.innerText = currency.value;
+  // calculate functions needed for convert function
 
-  if (operationToPLN.checked) {
-    amount.value == 0
-      ? (amountCurrencyField.innerText = `0.00 ${currency.value}`)
-      : (amountCurrencyField.innerText = `${amount.value} ${currency.value}`);
-    switch (currency.value) {
-      case "USD":
-        converterToPLN(amount.value, rateUSD);
-        break;
-      case "EUR":
-        converterToPLN(amount.value, rateEUR);
-        break;
-      case "GBP":
-        converterToPLN(amount.value, rateGBP);
-        break;
-      default:
-        alert("wystąpił błąd");
-        break;
+  const calculateIntoPLN = (amount, rate) => {
+    let result = amount * rate;
+    return result;
+  };
+  const calculateFromPLN = (amount, rate) => {
+    let result = amount / rate;
+    return result;
+  };
+
+  // convert function recognizes what currency we want to convert
+  const convert = () => {
+    const rateEUR = 4.7;
+    const rateGBP = 5.38;
+    const rateUSD = 4.82;
+    if (operationToPLN.checked) {
+      switch (currency.value) {
+        case "USD":
+          return calculateIntoPLN(amount.value, rateUSD);
+        case "EUR":
+          return calculateIntoPLN(amount.value, rateEUR);
+        case "GBP":
+          return calculateIntoPLN(amount.value, rateGBP);
+      }
+    } else if (operationFromPLN.checked) {
+      switch (currency.value) {
+        case "USD":
+          return calculateFromPLN(amount.value, rateUSD);
+        case "EUR":
+          return calculateFromPLN(amount.value, rateEUR);
+        case "GBP":
+          return calculateFromPLN(amount.value, rateGBP);
+      }
     }
-  } else if (operationFromPLN.checked) {
-    amount.value == 0
-      ? (amountCurrencyField.innerText = "0.00 PLN")
-      : (amountCurrencyField.innerText = `${amount.value} PLN`);
-    switch (currency.value) {
-      case "USD":
-        converterFromPLN(amount.value, rateUSD, currency.value);
-        break;
-      case "EUR":
-        converterFromPLN(amount.value, rateUSD, currency.value);
-        break;
-      case "GBP":
-        converterFromPLN(amount.value, rateUSD, currency.value);
-        break;
-      default:
-        alert("wystąpił błąd");
-        break;
-    }
-  } else {
-    alert("wystąpił błąd");
   }
-});
+
+  // type into field functions
+
+  const typeFormCurrencyField = () => {
+    const FormCurrencyField = document.querySelector(".js-FormCurrencyField");
+    FormCurrencyField.innerText = currency.value;
+  }
+
+  const typeResultField = () => {
+    const resultField = document.querySelector(".js-resultField");
+    if (operationToPLN.checked) {
+      resultField.innerText = `${convert().toFixed(2)} PLN`;
+    } else if (operationFromPLN.checked) {
+      resultField.innerText = `${convert().toFixed(2)} ${currency.value}`;
+    }
+  }
+
+  const typeAmountCurrencyField = () => {
+    const amountCurrencyField = document.querySelector(".js-amountCurrencyField");
+    if (operationToPLN.checked) {
+      amount.value == 0
+        ? (amountCurrencyField.innerText = `0.00 ${currency.value}`)
+        : (amountCurrencyField.innerText = `${amount.value} ${currency.value}`);
+    } else if (operationFromPLN.checked) {
+      amount.value == 0
+        ? (amountCurrencyField.innerText = "0.00 PLN")
+        : (amountCurrencyField.innerText = `${amount.value} PLN`);
+    }
+  }
+
+  //event function
+
+  const formElementInput = () => {
+    typeFormCurrencyField();
+    convert();
+    typeResultField();
+    typeAmountCurrencyField();
+  }
+
+  //main function
+  init = () => {
+    formElement.addEventListener("input", formElementInput);
+    console.log("Witaj w konsoli, mało kto tu zagląda ;O")
+  }
+
+  init();
+}
